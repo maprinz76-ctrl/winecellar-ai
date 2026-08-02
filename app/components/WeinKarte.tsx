6d0af9d3ca4bb966d9fec42191ea4b37b2cf1bf1
Marco Prinz <maprinz76@gmail.com>
2026-08-02 19:09:53 +0200

    WeinKarte vorbereitet

--- app/components/WeinKarte.tsx
+++ app/components/WeinKarte.tsx
@@ -1,3 +1,4 @@
+"use client";
 type Wein = {
   id: number;
   produzent: string;
@@ -14,13 +15,46 @@ type Wein = {
 
 type Props = {
   wein: Wein;
+  bestandAendern: (id: number, veraenderung: number) => void;
+  bewertungAendern: (id: number, sterne: number) => void;
+  weinLoeschen: (id: number) => void;
 };
 
-export default function WeinKarte({ wein }: Props) {
+export default function WeinKarte({
+  wein,
+  bestandAendern,
+  bewertungAendern,
+  weinLoeschen,
+}: Props) {
+  const gesamtwert = wein.anzahl * wein.preis;
   return (
-    <article>
+  <article
+  style={{
+    backgroundColor: "white",
+    padding: "22px",
+    borderRadius: "16px",
+    boxShadow: "0 6px 20px rgba(40, 30, 30, 0.08)",
+    marginBottom: "20px",
+  }}
+>
       <h2>{wein.weinname}</h2>
       <p>{wein.produzent}</p>
+      {wein.bild && (
+  <img
+    src={wein.bild}
+    alt={wein.weinname}
+    style={{
+      width: "120px",
+      height: "180px",
+      objectFit: "contain",
+      display: "block",
+      margin: "15px auto",
+      borderRadius: "10px",
+      border: "1px solid #ddd",
+      backgroundColor: "white",
+    }}
+  />
+)}
     </article>
   );
 }
